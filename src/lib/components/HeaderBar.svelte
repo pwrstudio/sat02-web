efa74a7b007829d3a608486529f9c11533721b0e
pwrstudio <pwrpntr@gmail.com>
2023-08-08 16:36:13 +0200

    add svg test

--- src/lib/components/HeaderBar.svelte
+++ src/lib/components/HeaderBar.svelte
@@ -1,9 +1,23 @@
-<div class="header-bar">XXX</div>
+<script lang="ts">
+  import dots from "$lib/assets/dots3.svg"
+</script>
+
+<div class="header-bar">
+  <img src={dots} alt="dots" />
+</div>
 
 <style lang="scss">
   .header-bar {
     height: var(--header-height);
     background: var(--orange);
     width: 100%;
+    overflow: hidden;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
   }
 </style>
--- src/lib/components/MenuBar.svelte
+++ src/lib/components/MenuBar.svelte
@@ -36,6 +36,7 @@
     padding-left: var(--default-padding);
     padding-right: var(--double-padding);
     justify-content: space-between;
+    z-index: 1000;
   }
 
   a {
