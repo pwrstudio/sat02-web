7c534946f55b7de3efae8c688d922bf9418b4382
pwrstudio <pwrpntr@gmail.com>
2023-10-30 14:46:41 +0100

    fix: press ordering

--- src/routes/press/+page.ts
+++ src/routes/press/+page.ts
@@ -3,7 +3,7 @@ import { loadData } from "$lib/modules/sanity.js"
 /** @type {import('./$types').PageLoad} */
 export async function load() {
     const page = await loadData("*[_id == 'press'][0]", {})
-    const pressReleases = await loadData("*[_type == 'pressRelease']{..., 'pdf_url': pdf.asset->url} | order(date)", {})
-    const pressCoverage = await loadData("*[_type == 'pressCoverage']{..., 'pdf_url': pdf.asset->url} | order(date)", {})
+    const pressReleases = await loadData("*[_type == 'pressRelease']{..., 'pdf_url': pdf.asset->url} | order(date desc)", {})
+    const pressCoverage = await loadData("*[_type == 'pressCoverage']{..., 'pdf_url': pdf.asset->url} | order(date desc)", {})
     return { page, pressReleases, pressCoverage };
 }
